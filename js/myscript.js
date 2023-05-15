65b2d118c99f407bd1b13f80a6402f6e8c9f82b7
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-05-15 17:08:36 +0200

    new try

--- js/myscript.js
+++ js/myscript.js
@@ -1,23 +1,24 @@
-const userAge = parseInt;
-const distance = parseInt;
+const inputAge = document.getElementById("user-age");
+console.log(inputAge)
+const inputNumber = document.getElementById("distance");
+console.log(inputNumber)
+let price = 0.23;
 
-let price = distance * 0.23;
 
-if ( userAge < 18){
-    price = price - (price * (19.4 / 100));
-    console.log(price.toFixed(2));
-} else if ( userAge >= 65) {
-    price = price - (price * (37.7 / 100));
-    console.log(price.toFixed(2));
-}
+
 
 const button =document.querySelector("button");
 console.log(button);
 
 button.addEventListener("click", function() {
-    price = price - (price * (37.7 / 100));
+    
+    if ( inputAge < 18){
+        price = price - (price * (19.4 / 100));
+        console.log(price.toFixed(2));
+    } else if ( inputAge >= 65) {
+        price = price - (price * (37.7 / 100));
+        console.log(price.toFixed(2));
+    }
 }
 )
 
-console.log("price");
-console.log("userAge");
