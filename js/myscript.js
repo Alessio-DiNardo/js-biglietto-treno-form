a7f9cdda8a198f74aa87358cb9ec4de946b2e580
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-05-15 18:08:25 +0200

    add value

--- js/myscript.js
+++ js/myscript.js
@@ -2,7 +2,7 @@ const inputAge = document.getElementById("user-age");
 console.log(inputAge)
 const inputNumber = document.getElementById("distance");
 console.log(inputNumber)
-let price = 0.23;
+let priceforchilometer = 0.23;
 
 
 
@@ -11,15 +11,17 @@ const button =document.querySelector("button");
 console.log(button);
 
 button.addEventListener("click", function() {
-    let price = 0.23 * distance;
-    console.log(distance)
-    if ( inputAge < 18){
+    let distance = inputNumber.value;
+    let price = priceforchilometer * distance;
+    console.log(price)
+    if ( inputAge.value < 18){
         price = price - (price * (19.4 / 100));
         console.log(price.toFixed(2));
-    } else if ( inputAge >= 65) {
+    } else if ( inputAge.value >= 65) {
         price = price - (price * (37.7 / 100));
         console.log(price.toFixed(2));
     }
+    document.getElementById().innerHTML = price.toFixed(2);
 }
 )
 
