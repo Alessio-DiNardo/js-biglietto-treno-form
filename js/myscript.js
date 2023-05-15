deca20d857ded9767fbdef4c64b526405fce1357
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-05-15 17:18:52 +0200

    price in eventlistener

--- js/myscript.js
+++ js/myscript.js
@@ -11,7 +11,8 @@ const button =document.querySelector("button");
 console.log(button);
 
 button.addEventListener("click", function() {
-    
+    let price = 0.23 * distance;
+    console.log(distance)
     if ( inputAge < 18){
         price = price - (price * (19.4 / 100));
         console.log(price.toFixed(2));
