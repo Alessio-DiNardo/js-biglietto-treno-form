100d109c56572d88eb2424e108980782c9a67fc1
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-05-15 16:09:48 +0200

    try to use add event listener

--- js/myscript.js
+++ js/myscript.js
@@ -3,10 +3,21 @@ const distance = parseInt;
 
 let price = distance * 0.23;
 
-if ( age < 18){
+if ( userAge < 18){
     price = price - (price * (19.4 / 100));
-} else if ( age >= 65) {
+    console.log(price.toFixed(2));
+} else if ( userAge >= 65) {
     price = price - (price * (37.7 / 100));
+    console.log(price.toFixed(2));
 }
 
-document.getElementById("output").innerHTML = price + "€";
+const button =document.querySelector("button");
+console.log(button);
+
+button.addEventListener("click", function() {
+    price = price - (price * (37.7 / 100));
+}
+)
+
+console.log("price");
+console.log("userAge");
